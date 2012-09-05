bdb949f91049ae69ad9205af9418150be6258ece
Eric West <esw9999@gmail.com>
2012-09-05 00:59:27 -0500

    Modifies README

--- public/javascripts/app/models/ircBot.js
+++ public/javascripts/app/models/ircBot.js
@@ -1,9 +1,10 @@
-var __defineProperty = function(clazz, key, value) {
+(function() {
+  var __defineProperty = function(clazz, key, value) {
   if (typeof clazz.__defineProperty == 'function') return clazz.__defineProperty(key, value);
   return clazz.prototype[key] = value;
 },
-  __hasProp = {}.hasOwnProperty,
-  __extends =   function(child, parent) {
+    __hasProp = {}.hasOwnProperty,
+    __extends =   function(child, parent) {
     if (typeof parent.__extend == 'function') return parent.__extend(child);
     for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } 
     function ctor() { this.constructor = child; } 
@@ -14,47 +15,49 @@ var __defineProperty = function(clazz, key, value) {
     return child; 
 };
 
-App.IrcBot = (function(_super) {
-  var IrcBot;
+  App.IrcBot = (function(_super) {
+    var IrcBot;
 
-  function IrcBot() {
-    return IrcBot.__super__.constructor.apply(this, arguments);
-  }
+    function IrcBot() {
+      return IrcBot.__super__.constructor.apply(this, arguments);
+    }
 
-  IrcBot = __extends(IrcBot, _super);
+    IrcBot = __extends(IrcBot, _super);
 
-  IrcBot.field('channel', {
-    type: 'String'
-  });
-
-  IrcBot.field('server', {
-    type: 'String'
-  });
-
-  IrcBot.field('name', {
-    type: 'String'
-  });
-
-  IrcBot.after('create', 'init');
-
-  IrcBot.timestamps();
-
-  __defineProperty(IrcBot,  "init", function() {
-    var channel, client, name, server;
-    channel = this.get('channel');
-    server = this.get('server');
-    name = this.get('name');
-    client = new App.irc.Client(server, name, {
-      channels: [channel]
+    IrcBot.field('channel', {
+      type: 'String'
     });
-    return client.addListener('join', function(channel, who) {
-      this.channel = channel;
-      if (who !== "TowerBot") {
-        return client.say(channel, "Hello " + who + "! If you have a question, please be patient! It may take some time before someone sees it, but we will try our best to help you.");
-      }
+
+    IrcBot.field('server', {
+      type: 'String'
     });
-  });
 
-  return IrcBot;
+    IrcBot.field('name', {
+      type: 'String'
+    });
 
-})(Tower.Model);
+    IrcBot.after('create', 'init');
+
+    IrcBot.timestamps();
+
+    __defineProperty(IrcBot,  "init", function() {
+      var channel, client, name, server;
+      channel = this.get('channel');
+      server = this.get('server');
+      name = this.get('name');
+      client = new App.irc.Client(server, name, {
+        channels: [channel]
+      });
+      return client.addListener('join', function(channel, who) {
+        this.channel = channel;
+        if (who !== "TowerBot") {
+          return client.say(channel, "Hello " + who + "! If you have a question, please be patient! It may take some time before someone sees it, but we will try our best to help you.");
+        }
+      });
+    });
+
+    return IrcBot;
+
+  })(Tower.Model);
+
+}).call(this);
--- public/javascripts/test/models/ircBotTest.js
+++ public/javascripts/test/models/ircBotTest.js
@@ -1,55 +1,58 @@
-var ircBot;
+(function() {
+  var ircBot;
 
-ircBot = null;
+  ircBot = null;
 
-describe("App.IrcBot", function() {
-  describe("fields", function() {
-    beforeEach(function(done) {
-      ircBot = App.IrcBot.build({
-        channel: "A channel",
-        server: "A server",
-        name: "A name"
-      });
-      return done();
-    });
-    test("channel", function() {
-      return assert.ok(ircBot.get("channel"));
-    });
-    test("server", function() {
-      return assert.ok(ircBot.get("server"));
-    });
-    return test("name", function() {
-      return assert.ok(ircBot.get("name"));
-    });
-  });
-  return describe("methods", function() {
-    return describe('init', function() {
+  describe("App.IrcBot", function() {
+    describe("fields", function() {
       beforeEach(function(done) {
         ircBot = App.IrcBot.build({
-          channel: "bottest123",
-          server: 'localhost',
-          name: "coolBot"
+          channel: "A channel",
+          server: "A server",
+          name: "A name"
         });
         return done();
       });
-      it("should call init on creation", function(done) {
-        var spy;
-        spy = sinon.spy(ircBot, "init");
-        ircBot.save();
-        spy.should.have.been.calledOnce;
-        return done();
+      test("channel", function() {
+        return assert.ok(ircBot.get("channel"));
       });
-      return it("should connect to the irc server", function(done) {
-        var server;
-        ircBot.save();
-        server = net.createServer();
-        server.listen(6667, 'localhost');
-        return server.on('connection', function(socket) {
-          socket.end();
-          server.close();
+      test("server", function() {
+        return assert.ok(ircBot.get("server"));
+      });
+      return test("name", function() {
+        return assert.ok(ircBot.get("name"));
+      });
+    });
+    return describe("methods", function() {
+      return describe('init', function() {
+        beforeEach(function(done) {
+          ircBot = App.IrcBot.build({
+            channel: "bottest123",
+            server: 'localhost',
+            name: "coolBot"
+          });
           return done();
+        });
+        it("should call init on creation", function(done) {
+          var spy;
+          spy = sinon.spy(ircBot, "init");
+          ircBot.save();
+          spy.should.have.been.calledOnce;
+          return done();
+        });
+        return it("should connect to the irc server", function(done) {
+          var server;
+          ircBot.save();
+          server = net.createServer();
+          server.listen(6667, 'localhost');
+          return server.on('connection', function(socket) {
+            socket.end();
+            server.close();
+            return done();
+          });
         });
       });
     });
   });
-});
+
+}).call(this);
